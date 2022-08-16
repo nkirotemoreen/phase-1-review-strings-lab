5a93e33041ee5289c301e147048ac2d2d95577ad
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-16 06:05:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Moreen Isaac";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//exctedWelcome message
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//shortGreeting"Welcome, ";
+ const shortGreeting = `Welcome, ${currentUser[0]}!`;
+ 
